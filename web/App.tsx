b029c3c1ebfd85cb500ddb24c71f4e496ea485bc
Pedro Henrique Ribeiro <126246097+pedrohenribeiro@users.noreply.github.com>
2025-04-21 14:45:39 -0300

    feat: initial layout

--- web/App.tsx
+++ web/App.tsx
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Menu from './src/components/layout/Menu';
+import Navbar from './src/components/layout/Navbar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+              <Menu />
+
+      <View style={styles.mainContent}>
+      <Navbar />
+        <View style={styles.pageContent}>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row', // vertical layout
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  mainContent: {
+    flex: 1,
+    flexDirection: 'column', // horizontal layout: menu + page content
+  },
+  pageContent: {
+    flex: 1, // ocupa o espaço restante
+    backgroundColor: '#f0f0f0',
+    padding: 10,
   },
 });
