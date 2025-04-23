8ab4a231a0e59022464cd8501679eba4b052018d
Pedro <pedro.ribeiro33@fatec.sp.gov.br>
2025-04-22 22:04:05 -0300

    feat: additional routes

--- web/src/routes/routes.tsx
+++ web/src/routes/routes.tsx
@@ -7,7 +7,11 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 export function Routes() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {/* <Stack.Screen name="Despesas" component={ListaDespesa} /> */}
+      {/* <Stack.Screen name="Funcionarios" component={Funcionarios} /> */}
       <Stack.Screen name="Categorias" component={Categoria} />
+      {/* <Stack.Screen name="Projetos" component={Projetos} /> */}
+      {/* <Stack.Screen name="Departamentos" component={Departamento} /> */}
     </Stack.Navigator>
   );
 }
